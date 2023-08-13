2bf1ab453e753e011b5db3571727a70056c88dfc
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-08-13 12:31:10 +0700

    start project setup vite react, taliwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center flex-row justify-center h-screen w-screen bg-[#f5f5f5]">
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
