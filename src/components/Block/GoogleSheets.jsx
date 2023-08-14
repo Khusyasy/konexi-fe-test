1acc8bf421628ade6798dc8ec224ba4e4a9c672f
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-08-14 09:17:05 +0700

    flow state button

--- src/components/Block/GoogleSheets.jsx
+++ src/components/Block/GoogleSheets.jsx
@@ -1,8 +1,12 @@
+import { useState } from 'react';
+
 import BlockBase from './BlockBase';
+import ConnectGoogle from '../ConnectGoogle';
+
+import Button from '../Button';
 
 import GoogleSheetsLogo from '../../icons/GoogleSheetsLogo';
-import ConnectGoogle from '../ConnectGoogle';
-import { useState } from 'react';
+import FlowIcon from '../../icons/FlowIcon';
 
 const STEPS = {
   CONNECT_GOOGLE: 0,
@@ -11,10 +15,21 @@ const STEPS = {
 };
 
 function GoogleSheets() {
-  const [step, setStep] = useState(STEPS.CONNECT_GOOGLE);
+  // const [step, setStep] = useState(STEPS.CONNECT_GOOGLE);
+  const [step, setStep] = useState(STEPS.CONNECT_FLOW);
+  const [loading, setLoading] = useState(false);
 
   function connectGoogleDone() {
     setStep(STEPS.CONNECT_FLOW);
+  }
+
+  function connectFlow() {
+    setLoading(true);
+    setTimeout(() => {
+      // mock api call stuff
+      setLoading(false);
+      setStep(STEPS.EXPORT_SHEET);
+    }, 3 * 1000);
   }
 
   return (
@@ -22,7 +37,21 @@ function GoogleSheets() {
       {step === STEPS.CONNECT_GOOGLE && (
         <ConnectGoogle done={connectGoogleDone} />
       )}
-      {step === STEPS.CONNECT_FLOW && <div>Connect Flow</div>}
+      {step === STEPS.CONNECT_FLOW && (
+        <div className="pt-6">
+          <Button
+            full
+            rounded
+            variant="secondary"
+            className="h-16 max-h-16"
+            onClick={connectFlow}
+            loading={loading}
+          >
+            <FlowIcon className="text-gray-500" />
+            Connect Flow Node to Import to Google Sheets
+          </Button>
+        </div>
+      )}
       {step === STEPS.EXPORT_SHEET && <div>Export Sheet</div>}
     </BlockBase>
   );
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,16 +5,25 @@ function Button({
   full = false,
   rounded = false,
   loading = false,
+  variant = 'primary',
+  className,
   children,
   ...props
 }) {
-  const btnClass = classNames({
-    'flex flex-row gap-2 justify-center items-center px-5 py-2 h-12 max-h-12" bg-[#2284f3] text-white text-xl/[1] font-bold hover:bg-[#1e6ab3] hover:text-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50 transition-all': true,
-    'w-full': full,
-    'w-32': !full,
-    'rounded-full': rounded,
-    'rounded-lg': !rounded,
-  });
+  const btnClass = classNames(
+    {
+      'flex flex-row gap-2 justify-center items-center px-5 py-2 h-12 max-h-12 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50 transition-all': true,
+      'bg-[#2284f3] text-white text-xl/[1] font-bold hover:bg-[#1e6ab3] hover:text-gray-50':
+        variant === 'primary',
+      'bg-[#f0f0f0] text-gray-700 text-xl/[1] font-semibold hover:bg-[#e2e2e2] hover:text-gray-800':
+        variant === 'secondary',
+      'w-full': full,
+      'w-32': !full,
+      'rounded-full': rounded,
+      'rounded-lg': !rounded,
+    },
+    className
+  );
 
   return (
     <button {...props} className={btnClass}>
