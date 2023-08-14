82acaf5af42daa1ac8619523305a42797e277246
Luthfi Khusyasy <44360105+Khusyasy@users.noreply.github.com>
2023-08-14 14:18:55 +0700

    add search and filter sheet tabs

--- src/components/Block/GoogleSheets/SheetsDropdown.jsx
+++ src/components/Block/GoogleSheets/SheetsDropdown.jsx
@@ -1,9 +1,30 @@
-import { Fragment } from 'react';
+import { Fragment, useMemo, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
-import { ChevronDownIcon, CheckIcon } from '@heroicons/react/20/solid';
+import {
+  ChevronDownIcon,
+  CheckIcon,
+  MagnifyingGlassIcon,
+} from '@heroicons/react/20/solid';
 
 function defaultChildren(item) {
   return item;
+}
+
+function SearchInput({ value, onChange }) {
+  return (
+    <div className="relative">
+      <span className="absolute inset-y-0 left-0 flex items-center px-3">
+        <MagnifyingGlassIcon className="h-5 w-5 text-gray-400 z-20" />
+      </span>
+      <input
+        type="text"
+        className="w-full h-12 block relative p-4 pl-10 bg-white border border-gray-300 text-black text-left text-base/[1] rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50"
+        value={value}
+        onChange={onChange}
+        placeholder="Search"
+      />
+    </div>
+  );
 }
 
 function SheetsDropdown({
@@ -12,6 +33,16 @@ function SheetsDropdown({
   setSelectedItem,
   children = defaultChildren,
 }) {
+  const [search, setSearch] = useState('');
+  const filteredItems = useMemo(() => {
+    if (!search) {
+      return items;
+    }
+    return items.filter((item) =>
+      item.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [items, search]);
+
   return (
     <Listbox value={selectedItem} onChange={setSelectedItem}>
       <div className="relative">
@@ -33,8 +64,17 @@ function SheetsDropdown({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Listbox.Options className="absolute mt-2 w-64 max-h-60 min-w-full overflow-auto rounded-lg bg-white py-1 text-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
-            {items.map((item, itemIdx) => (
+          <Listbox.Options className="absolute mt-2 w-64 max-h-64 min-w-full overflow-auto rounded-lg bg-white py-1 text-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10 pt-[72px]">
+            <div className="absolute inset-0 p-3">
+              <SearchInput
+                value={search}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setSearch(e.target.value);
+                }}
+              />
+            </div>
+            {filteredItems.map((item, itemIdx) => (
               <Listbox.Option
                 key={itemIdx}
                 className={({ active }) =>
